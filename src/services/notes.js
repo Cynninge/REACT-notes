badadb3317e5542c99a2462e0f2752a427d97b3f
Adam Garbacz <gaad77@gmail.com>
2021-02-11 17:24:45 +0100

    REACT and Redux notes.Frontend

---
+++ src/services/notes.js
@@ -0,0 +1,46 @@
+import {ActionCreators } from "../redux/notesReducer";
+import  * as axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://localhost:5001/notes',
+})
+
+export const GetNotes = async(dispatch) => {
+    try {
+        const {data} = await axiosInstance.get();
+        dispatch(ActionCreators.setNotes(data));
+    }
+    catch {
+        console.log('error');
+    }
+}
+
+export const DeleteNote = async(dispatch, note) => {
+    try {
+        await axiosInstance.delete(`/${note.id}`);
+        dispatch(ActionCreators.deleteNote(note));
+    }
+    catch {
+        console.log('error');
+    }
+}
+
+export const NewNote = async(dispatch, note) => {
+    try {
+        const {data} = await axiosInstance.post('', note);
+        dispatch(ActionCreators.newNote(data));
+    }
+    catch {
+        console.log('error');
+    }
+}
+
+export const EditNote = async(dispatch, note) => {
+    try {
+        await axiosInstance.put('', note);
+        dispatch(ActionCreators.editNote(note));
+    }
+    catch {
+        console.log('error');
+    }
+}
